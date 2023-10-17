099b3e3f7dbb2bbabd3bd8439c6170fe23809015
seokmin1 <dbstjrals130@naver.com>
2023-10-17 15:13:44 +0900

    다시

--- mydb/subquery.sql
+++ mydb/subquery.sql
@@ -4,7 +4,7 @@
 -- 도서 중 가격이 가장 높은 가격을 검색하시오
 -- MAX(price) : 35000원
 SELECT MAX(price) FROM book;
-
+-- 단일행 서브쿼리
 -- 도서 중에서 가장 비싼 도서의 이름을 검색하시오
 SELECT bookname, price
 FROM book
@@ -16,10 +16,57 @@ SELECT custid FROM orders;
 
 SELECT name
 FROM customer
-WHERE custid IN(SELECT custid FROM orders);
+WHERE custid = 3;
+
+-- 다중행 서브쿼리
+-- 도서를 구매한 적이 있는 고객의 이름 출력
+SELECT name
+FROM customer
+WHERE custid IN (SELECT custid FROM orders);
 
 -- 조인 방식 : customer, orders
 -- 도서를 구매한 적이 있는 고객의 이름을 검색하시오
 SELECT DISTINCT cus.name
 FROM customer cus, orders ord
 WHERE cus.custid = ord.custid;
+
+-- FROM절에 있는 서브쿼리 - 인라인뷰라고도 함
+-- 고객의 이름과 고객의 판매액 검색(customer, orders)
+-- 그룹별 : Group by
+-- 단, 고객 번호가 2 이하임
+
+-- 동등 조인
+SELECT cs.name, SUM(od.saleprice)
+FROM customer cs, orders od
+WHERE cs.custid = od.custid
+  AND cs.custid <= 2
+GROUP BY cs.name
+  HAVING SUM(od.saleprice) >= 30000;
+  
+/*
+  실행 순서
+  1. FROM절 테이블명
+  2. WHERE, GROUP BY
+  3. SELECT 절
+  4. ORDER 절
+*/
+
+-- 서브 쿼리
+SELECT cs.name, SUM(od.saleprice)
+FROM (SELECT * FROM customer
+        WHERE custid <=2) cs, 
+     orders od
+WHERE cs.custid = od.custid
+GROUP BY cs.name;
+
+SELECT * FROM product;
+SELECT * FROM product_review;
+-- 스칼라 서브쿼리 : select 절에 있는 select문을 말함
+-- 상품 리뷰 테이블에 없는 상품 이름 검색
+SELECT a.product_code,
+       (select b.product_name
+         from product b
+         where a.product_code = b.product_code) product_name,
+       a.member_id,
+       a.content
+FROM product_review a;

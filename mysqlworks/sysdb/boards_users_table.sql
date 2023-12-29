4b8c3c2f6078e73d6bde231db0c005eda9c79773
seokmin1 <dbstjrals130@naver.com>
2023-12-29 18:17:45 +0900

    dbstjrals130

--- mysqlworks/sysdb/boards_users_table.sql
+++ mysqlworks/sysdb/boards_users_table.sql
@@ -11,3 +11,17 @@ create table users(
 );
 
 drop table if exists users;
+
+use springdb;
+
+create table boards(
+	id bigint primary key auto_increment,
+    boardtitle varchar(100) not null,
+    boardwriter varchar(20) not null,
+    boardcontent text not null,
+    hit int default 0,
+    createtime datetime default now(),
+    updatedtime datetime
+);
+
+select * from boards;
